a53a09bce074cb4727eccda21cbec07556084245
Taylor Jackle Spriggs <74561858+tjs-intel@users.noreply.github.com>
2023-04-04 15:17:25 -0600

    Custom bash lint (#6)
    * copy in bash lint
    
    * sh alternative to bash
    
    * testing
    
    * test wrong shebang
    
    * manually add disclaimer

---
+++ bash_lint.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# Copyright © 2022 Intel Corporation
+#
+# SPDX-License-Identifier: Apache License 2.0
+
+import argparse
+import sys
+
+
+required_directives = [
+  ("set -e", ("set +e", "# no_set_e")),
+  ("set -o pipefail", ("set +o pipefail", "# no_pipefail")),
+  ("#!/usr/bin/env bash", ("#!/usr/bin/env sh",)),
+]
+
+
+def bash_lint(file):
+  lines = file.readlines()
+  for directive, alternatives in required_directives:
+    for line in lines:
+      line = line.strip()
+      if line == directive or line in alternatives:
+        break
+    else:
+      yield f"error: Missing `{directive}` directive."
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("files", type=str, nargs="+", help="File to lint")
+  args = parser.parse_args()
+  responses = []
+  for path in args.files:
+    with open(path) as fp:
+      responses.extend([f"{path}: {error}" for error in bash_lint(fp)])
+  if responses:
+    print("\n".join(responses))  # noqa: T001
+    sys.exit(1)
---
+++ test/bash_lint_test.py
@@ -0,0 +1,54 @@
+# Copyright © 2022 Intel Corporation
+#
+# SPDX-License-Identifier: Apache License 2.0
+
+import io
+
+import pytest
+
+from bash_lint import bash_lint
+
+
+@pytest.mark.parametrize(
+  "shebang,shebang_issues",
+  [
+    ("#!/usr/bin/env bash", 0),
+    ("#!/usr/bin/env sh", 0),
+    ("#!/usr/bin/env python3", 1),
+    ("", 1),
+  ],
+)
+@pytest.mark.parametrize(
+  "sete,sete_issues",
+  [
+    ("set -e", 0),
+    ("set +e", 0),
+    ("# no_set_e", 0),
+    ("", 1),
+  ],
+)
+@pytest.mark.parametrize(
+  "pipefail,pipefail_issues",
+  [
+    ("set -o pipefail", 0),
+    ("# no_pipefail", 0),
+    ("", 1),
+  ],
+)
+@pytest.mark.parametrize(
+  "script",
+  [
+    "echo testing\n",
+  ],
+)
+def test_bash_lint(shebang, shebang_issues, sete, sete_issues, pipefail, pipefail_issues, script):
+  content = "\n".join(
+    [
+      shebang,
+      sete,
+      pipefail,
+      script,
+    ]
+  )
+  file = io.StringIO(content)
+  assert len(list(bash_lint(file))) == sum([shebang_issues, sete_issues, pipefail_issues])
